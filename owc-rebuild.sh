9b8dc69352e42b626a475e319ebe3e4adf94fcc9
Chris Weber <cweber@nerdery.com>
2011-07-13 15:22:30 -0500

    remove client info from script

--- owc-rebuild.sh
+++ owc-rebuild.sh
@@ -1,3 +1,19 @@
-lps/lps.sh -s drush -m lps -n lifetouch -p lifetouch -d LPS_cweber
+# OPTIONS:
+#   -h      Show this message
+#
+# DATABASE SETTINGS
+#   -n      Database Username
+#   -p      Database Password
+#   -d      Database Name
+#   
+# DRUSH
+#   -s      Path to drush install.  The script assumes this is somewhere within your home directory.
+#           For example, if installed in ~/drush, this would be set as such: -s drush
+#
+# PROJECT SPECIFIC OPTIONS
+#   -w     Project Name (This will be the directory within your public_html directory)
+#   -m     Project Path (Directory within your home directory where the distro.make drush makefile exists)
+#   -p     Machine name of the install profile with which to install Drupal.
+#
 
-
+owc/build.sh -s drush -m owc -n user -p pass -d OWC_cweber
